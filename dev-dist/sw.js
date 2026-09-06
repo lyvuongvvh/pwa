1fc991d283681ad734d51641be0daaac01b98d69
lyvuongvvh <lyvuong@viethoc.com>
2026-09-06 19:26:07 +0000

    Update service worker cache revision

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.f2dks93ta7g"
+    "revision": "0.ugov6oe86go"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
